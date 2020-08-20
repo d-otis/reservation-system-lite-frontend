e1c1b1244aa90b7d1080eb8c3271e32dd3676f84
d-otis <dan@dan-foley.com>
2020-08-20 19:12:11 -0400

    tweak Item constructor to get at JSON better

--- src/item.js
+++ src/item.js
@@ -1,15 +1,17 @@
 class Item {
-  constructor() {
-    debugger
-    this.id = id
-    this.title = title
-    this.description = description
-    this.serialNumber = serialNumber
-    this.isAvailable = isAvailable
+  constructor(obj) {
+    // debugger
+    this.id = obj.id
+    this.title = obj.attributes.title
+    this.description = obj.attributes.description
+    this.serialNumber = obj.attributes.serial_number
+    this.isAvailable = obj.attributes.is_available
   }
 
   static get(e) {
     e.preventDefault()
     fetch()
   }
+
+
 }
