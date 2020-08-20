2dc9d3bb439ca6c54f5352bf9d6f00b8e83dd637
d-otis <dan@dan-foley.com>
2020-08-20 17:30:21 -0400

    define Item constructor

---
+++ src/item.js
@@ -0,0 +1,14 @@
+class Item {
+  constructor() {
+    debugger
+    this.id = id
+    this.title = title
+    this.description = description
+    this.serialNumber = serialNumber
+    this.isAvailable = isAvailable
+  }
+
+  static get() {
+    fetch()
+  }
+}
