96a21e2a7a560fe69ef2c003b7b8d5444ee52a2e
d-otis <dan@dan-foley.com>
2020-08-20 17:14:53 -0400

    define Navbar.render()

---
+++ src/navbar.js
@@ -0,0 +1,56 @@
+class Navbar {
+  constructor() {
+
+  }
+
+  static render() {
+    // DECLARATIONS + ASSIGNMENTS
+    const nav = document.createElement('nav')
+    const brand = document.createElement('a')
+    const hamburger = document.createElement('button')
+    const icon = document.createElement('span')
+    const collapseContainer = document.createElement('div')
+    const collapseInner = document.createElement('div')
+    const itemsLink = document.createElement('a')
+    const reservationsLink = document.createElement('a')
+
+    // ATTRS + CLASSES
+    // NAV STUFF
+    nav.classList.add('navbar', 'navbar-expand-md', 'navbar-light', 'bg-light')
+
+    brand.classList.add('navbar-brand')
+    brand.innerText = "Reservation System"
+
+    hamburger.classList.add('navbar-toggler')
+    hamburger.type = 'button'
+    hamburger.dataset.toggle = 'collapse'
+    hamburger.dataset.target = '#navbarNavAltMarkup'
+
+    icon.classList.add("navbar-toggler-icon")
+
+    collapseContainer.classList.add("collapse", "navbar-collapse")
+    collapseContainer.id = 'navbarNavAltMarkup'
+
+    collapseInner.classList.add('navbar-nav')
+
+    // ITEMS
+    itemsLink.classList.add('nav-item', 'nav-link')
+    itemsLink.innerText = "Items"
+    itemsLink.id = "items-link"
+
+    // RESERVATIONS
+    reservationsLink.classList.add('nav-item', 'nav-link')
+    reservationsLink.innerText = "Reservation"
+    reservationsLink.id = "reservations-link"
+
+    // APPEND
+    root.appendChild(nav)
+    nav.appendChild(brand)
+    nav.appendChild(hamburger)
+    hamburger.appendChild(icon)
+    nav.appendChild(collapseContainer)
+    collapseContainer.appendChild(collapseInner)
+    collapseInner.appendChild(itemsLink)
+    collapseInner.appendChild(reservationsLink)
+  }
+}
