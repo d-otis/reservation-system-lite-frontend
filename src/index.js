3703b2b024ac9aebe683be83a159dcfc14876ce8
d-otis <dan@dan-foley.com>
2020-08-21 11:06:24 -0400

    navbar styling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const root = document.querySelector('#root')
 const header = document.querySelector('header')
+header.classList.add('mb-5', 'shadow-sm')
 root.classList.add('container-fluid')
 
 Navbar.render()
