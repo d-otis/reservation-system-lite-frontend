47435a6944fb8aad65fc0869d4fd75c73364b89e
d-otis <dan@dan-foley.com>
2020-08-23 13:14:18 -0400

    define rendering functions for all Reservations + Reservation members

--- src/reservation.js
+++ src/reservation.js
@@ -46,7 +46,91 @@ class Reservation {
   }
 
   static renderAll(reservations) {
-    debugger
+    // DECLARATIONS + ASSIGNMENTS
+    const table = document.createElement("table")
+    const thead = document.createElement("thead")
+    const headersRow = document.createElement("tr")
+    const headerId = document.createElement("th")
+    const headerNotes = document.createElement("th")
+    const headerNumItems = document.createElement("th")
+    const headerShow = document.createElement("th")
+    const headerEdit = document.createElement("th")
+    const headerDelete = document.createElement("th")
+    const tbody = document.createElement("tbody")
+
+    // ELEMENT ATTRIBUTES
+    table.id = "reservations-table"
+    table.classList.add('table', 'table-hover')
+    headerId.scope = "col"
+    headerNotes.scope = "col"
+    headerNumItems.scope = "col"
+    headerEdit.scope = "col"
+    headerDelete.scope = "col"
+
+    // HEADERS INNERTEXT
+    headerId.innerText = "Reservation ID"
+    headerNotes.innerText = "Notes"
+    headerNumItems.innerText = "Number of Items"
+    headerShow.innerText = "View"
+    headerEdit.innerText = "Edit"
+    headerDelete.innerText = "Delete"
+
+    // APPENDS
+    root.appendChild(table)
+    table.appendChild(thead)
+    thead.appendChild(headersRow)
+    headersRow.appendChild(headerId)
+    headersRow.appendChild(headerNotes)
+    headersRow.appendChild(headerNumItems)
+    headersRow.appendChild(headerShow)
+    headersRow.appendChild(headerEdit)
+    headersRow.appendChild(headerDelete)
+    table.appendChild(tbody)
+
+    for (const reservation of reservations) {
+      reservation.render(tbody)
+    }
+  }
+
+  render = function(tbody) {
+    // DECLARATIONS + ASSIGNMENTS
+    const tr = document.createElement('tr')
+    const thResId = document.createElement('th')
+    const resNotes = document.createElement('td')
+    const resNumItems = document.createElement('td')
+    const resShowTd = document.createElement('td')
+    const resEditTd = document.createElement('td')
+    const resDeleteTd = document.createElement('td')
+    const viewBtn = document.createElement('button')
+    const deleteBtn = document.createElement('button')
+    const editBtn = document.createElement('button')
+
+    // ELEMENT ATTRIBUTES/PROPERTIES
+    thResId.scope = 'row'
+    thResId.innerText = this.id
+    resNotes.innerText = this.notes
+    resNumItems.innerText = this.numItems
+    deleteBtn.classList.add('btn', 'btn-sm', 'btn-danger')
+    editBtn.classList.add('btn', 'btn-sm', 'btn-secondary')
+    deleteBtn.innerText = "Delete"
+    editBtn.innerText = "Edit"
+    deleteBtn.dataset.deleteReservation = this.id
+    editBtn.dataset.editReservation = this.id
+    viewBtn.innerText = 'View'
+    viewBtn.classList.add('btn', 'btn-sm', 'btn-primary')
+    viewBtn.dataset.viewReservation = this.id
+
+    // APPENDS
+    tbody.appendChild(tr)
+    tr.appendChild(thResId)
+    tr.appendChild(resNotes)
+    tr.appendChild(resNumItems)
+    tr.appendChild(resShowTd)
+    tr.appendChild(resEditTd)
+    tr.appendChild(resDeleteTd)
+    resShowTd.appendChild(viewBtn)
+    resEditTd.appendChild(editBtn)
+    resDeleteTd.appendChild(deleteBtn)
   }
 
   update = function() {
