a6a58a44292540676fa489667a685d0eb4570276
d-otis <dan@dan-foley.com>
2020-08-24 15:40:12 -0400

    declare ITEMS & RESERVATIONS variables @ top of the js-loading

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const root = document.querySelector('#root')
 const header = document.querySelector('header')
+const BASE_URL = "http://127.0.0.1:3000/api/v1"
+let ITEMS
+let RESERVATIONS
 header.classList.add('mb-5', 'shadow-sm')
 root.classList.add('container-fluid')
 
